ff2a7e2ce6ef35e21384c7ec12bc4e50a68e1cd0
Rafis Bulatov <rafbullstuff@gmail.com>
2024-06-08 22:34:45 +0300

    Создан пустой проект с одним контроллером

---
+++ MarvelApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MarvelApp
+//
+//  Created by Rafis on 08.06.2024.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
+
