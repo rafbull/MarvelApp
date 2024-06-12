89500266bc6bf7613f18b1dc6dee95235c09be37
Rafis Bulatov <rafbullstuff@gmail.com>
2024-06-12 11:01:02 +0300

    Добавлены AppColor и AppFont

---
+++ MarvelApp/Resources/AppColor.swift
@@ -0,0 +1,15 @@
+//
+//  AppColor.swift
+//  MarvelApp
+//
+//  Created by Rafis on 09.06.2024.
+//
+
+import UIKit
+
+enum AppColor {
+    static let background = UIColor.systemBackground
+    static let accentColor = UIColor.convertFromHex(0xE50B14, alpha: 1.0)
+    static let coverComicFontColor = UIColor.white
+    static let fontColor = UIColor.label
+}
---
+++ MarvelApp/Resources/AppFont.swift
@@ -0,0 +1,15 @@
+//
+//  AppFont.swift
+//  MarvelApp
+//
+//  Created by Rafis on 11.06.2024.
+//
+
+import UIKit
+
+enum AppFont {
+    static let title = UIFont.systemFont(ofSize: 28, weight: .semibold)
+    static let header = UIFont.systemFont(ofSize: 24, weight: .medium)
+    static let body = UIFont.systemFont(ofSize: 16, weight: .medium)
+    static let accessoryTitle = UIFont.systemFont(ofSize: 14, weight: .regular)
+}
