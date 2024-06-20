3c85264779009800b242e6b79c0c6101a7635b4c
Rafis Bulatov <rafbullstuff@gmail.com>
2024-06-20 17:37:11 +0300

    Создание модулей вынесено в приватный extension SceneDelegate

--- MarvelApp/Application/SceneDelegate.swift
+++ MarvelApp/Application/SceneDelegate.swift
@@ -8,67 +8,44 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
     var window: UIWindow?
-
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = createRootViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
+// MARK: - Private Extension
+private extension SceneDelegate {
+    func createRootViewController() -> UIViewController {
         let networkService = NetworkService()
+        let coreDataService = CoreDataService()
         
         let homeNavigationController = UINavigationController()
-        let homeDependencies = HomeAssembly.Dependencies(navigationController: homeNavigationController, networkService: networkService)
+        let homeDependencies = HomeAssembly.Dependencies(navigationController: homeNavigationController, coreDataService: coreDataService, networkService: networkService)
         let homeViewController = HomeAssembly.makeModule(with: homeDependencies)
         homeNavigationController.viewControllers = [homeViewController]
         
         let searchDataManager = SearchDataManager()
         let searchNavigationController = UINavigationController()
-        let searchDependencies = SearchAssembly.Dependencies(navigationController: searchNavigationController, dataManager: searchDataManager, networkService: networkService)
+        let searchDependencies = SearchAssembly.Dependencies(navigationController: searchNavigationController, dataManager: searchDataManager, coreDataService: coreDataService, networkService: networkService)
         let searchViewController = SearchAssembly.makeModule(with: searchDependencies)
         searchNavigationController.viewControllers = [searchViewController]
         
-//        let tabBarController = TabBarController(homeNavigationController, searchNavigationController, UIViewController())
-        #warning("поставить контроллеры в правильном порядке")
-        let tabBarController = TabBarController(searchNavigationController, homeNavigationController, UIViewController())
+        let libraryNavigationController = UINavigationController()
+        let libraryDependencies = LibraryAssembly.Dependencies(navigationController: libraryNavigationController, coreDataService: coreDataService, networkService: networkService)
+        let libraryViewController = LibraryAssembly.makeModule(with: libraryDependencies)
+        libraryNavigationController.viewControllers = [libraryViewController]
         
-        window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = tabBarController
-        window?.makeKeyAndVisible()
+        return TabBarController(
+            homeNavigationController,
+            searchNavigationController,
+            libraryNavigationController
+        )
     }
-
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
-    }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
-
-        // Save changes in the application's managed object context when the application transitions to the background.
-        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
-    }
-
-
 }
-
